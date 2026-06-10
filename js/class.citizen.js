2588ed02b1e138a67a3db49f544e762636b6c493
maikypi <s2510892022@fhooe.at>
2026-06-10 13:05:33 +0200

    Update class.citizen.js

--- js/class.citizen.js
+++ js/class.citizen.js
@@ -11,10 +11,15 @@
  *
  */
 
-export default class Citizen{
 
-    /* Math.random() gives a value from 0 to <1
-    3 scales it to 0–<3
-    Math.floor(...) makes it 0, 1, 2
-    + 2 shifts it to 2, 3, 4 */
+export default class Citizen {
+    constructor(name) {
+        this.name = name;
+        this.rank = Math.floor(Math.random() * 3) + 2;
+        this.home = null;
+    }
+
+    toString() {
+        return `${this.name}, Citizen, Rank ${this.rank}`;
+    }
 }
